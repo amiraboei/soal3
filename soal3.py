8c3b7ba8ed2377b04c0b3d76c988003798970d59
amiraboei <amiraboei1384@gmail.com>
2024-03-05 11:55:13 -0800

    soal3

---
+++ soal3.py
@@ -0,0 +1,13 @@
+number1 =int(input("enter your number:"))
+number2 = int(input("enter your number:"))
+n=input("chi kar konim?:")
+if n=="sum":
+    print("mosavi ba :",number1+number2)
+elif n=="difference":
+    print("mosavi ba :",number1-number2)
+elif n=="multiple":
+    print("mosavi ba :",number1*number2)
+elif n=="divide":
+    print("mosavi ba :",number1/number2)
+else:
+    print("chi migi")
